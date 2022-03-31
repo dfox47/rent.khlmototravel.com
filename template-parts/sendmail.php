41b9a4cc313c7731ec8a47267d5f326341599e9d
Dennis FOX <dfox@foxartbox.com>
2022-04-01 00:23:31 +0300

    Email subject cahnged.

--- template-parts/sendmail.php
+++ template-parts/sendmail.php
@@ -1,10 +1,8 @@
-<?php // Define constants
-const RECIPIENT_NAME = "rentkhlmototravel.com";
-const RECIPIENT_EMAIL = "a9942212@gmail.com"; // where to send email
-const EMAIL_SUBJECT = "[rent.khlmototravel.com]";
 
-//$sender_email			= isset( $_POST['feedback__email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['sender_email'] ) : "";
-//$sender_name			= isset( $_POST['feedback__name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['sender_name'] ) : "";
+<?php // Define constants
+const RECIPIENT_NAME        = "rentkhlmototravel.com";
+const RECIPIENT_EMAIL       = "a9942212@gmail.com";
+const EMAIL_SUBJECT         = "[rent.khlmototravel.com]";
 
 $order_bike                 = $_POST['bike'];
 $order_pickup_date          = $_POST['pickup_date'];
@@ -13,10 +11,8 @@ $order_biker_name           = $_POST['biker_name'];
 $order_biker_phone          = $_POST['biker_phone'];
 $order_biker_email          = $_POST['biker_email'];
 
-
-
 $message = "
-	<html>
+	<html lang='en'>
 		<body>
 			<p><strong>Bike:</strong> $order_bike</p>
 			<p><strong>Pickup:</strong> $order_pickup_date</p>
@@ -28,15 +24,13 @@ $message = "
 	</html>
 ";
 
-
-
 // If all values exist, send the email
 //if ($sender_name && $sender_email && $sender_company) {
 if ($order_bike) {
 	$recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
 	$headers = "Content-type: text/html; charset = utf-8 \r\n";
 	$headers .= "From: " . $order_biker_name . " <" . $order_biker_email . ">";
-	$success = mail($recipient, EMAIL_SUBJECT, $message, $headers);
+	$success = mail($recipient, EMAIL_SUBJECT . $order_bike, $message, $headers);
 }
 
 // Return an appropriate response to the browser
@@ -44,22 +38,20 @@ if (isset($_GET["ajax"])) {
 	echo $success ? "success" : "error";
 }
 else { ?>
-	<html>
+	<html lang="en">
 		<head>
 			<title>Thank you!</title>
 		</head>
 
 		<body>
-			<?php if ($success) {
-				echo "<p>Thank you for your message!</p>";
-			}
-			else {
-				echo "<p>Error while sendind, try again please</p>";
-			} ?>
+			<?php if ($success) { ?>
+				<p>Thank you for your message!</p>
+			<?php }
+			else { ?>
+				<p>Error while sendind, try again please</p>
+			<?php } ?>
 
 			<a href="/">To home page</a>
 		</body>
 	</html>
 <?php } ?>
-
-
